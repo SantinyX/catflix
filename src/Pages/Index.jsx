5b29adbdad753f618175d476b7ec2c37f76b09bf
SantinyX <diegosantiny@hotmail.com>
2022-07-18 22:25:04 -0300

    Erro do css

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,22 +1,20 @@
 import React from "react";
-
 import { Cards } from "../Layouts/Cards";
 import Footer from "../Layouts/Footer";
 import Header from "../Layouts/Header";
-import Style from "../Pages/Index.module.css"
+
 
 function Index() {    
 
     return (
-        <div className={Style.container}>
+        <div >
             <Header />
-            <div className={Style.Cards}>
-                <h2 className={Style.h2}>Filmes Populares</h2>
-                <ul className={Style.ul}>                    
+            <div >
+                <h2>Filmes Populares</h2>
+                <ul>                    
                     <li><Cards /></li>
                 </ul>
             </div>
-
             <strong><Footer /></strong>
         </div>
     )
